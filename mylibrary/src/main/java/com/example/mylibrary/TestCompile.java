26f9123d7319c48d126740c0df552aa95d1e59f8
maokaixin <1064057085@qq.com>
2018-05-24 16:43:39 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/TestCompile.java
@@ -0,0 +1,10 @@
+package com.example.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class TestCompile {
+    public static void myTestCompile(Context context){
+        Toast.makeText(context,"我的第一个测试compile依赖库",Toast.LENGTH_LONG).show();
+    }
+}
